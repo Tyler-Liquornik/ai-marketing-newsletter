49067c5c0e5d7ec196ba0f93b50fc2836ac96fb5
Tyler-L-UWO <tyler.liquornik@gmail.com>
2025-01-09 22:56:05 -0500

    trying out production only vite config instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ai-marketing-newsletter/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/ai-marketing-newsletter/", // Base URL for GitHub Pages
 });
